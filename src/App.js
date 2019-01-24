09a9f8871e7bf7399dfe81090602ab0c02f4c8f1
JapocoinYenHoang <japocoin@gmail.com>
2019-01-24 08:48:56 +0100

    done Api request

--- src/App.js
+++ src/App.js
@@ -2,6 +2,26 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      characters: []
+    }
+
+
+  }
+  componentDidMount(){
+    this.getCharacters();
+  }
+  getCharacters(){
+    const ENDPOIND='http://hp-api.herokuapp.com/api/characters/?results=25'
+
+    fetch(ENDPOIND)
+    .then (response=>response.json())
+    .then(data=>{
+      console.log (data)
+    })
+  }
   render() {
     return (
       <div className="App">
