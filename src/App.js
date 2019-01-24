f4bc33a2519b48aed6aa0b08d983bc6a9ac95c71
JapocoinYenHoang <japocoin@gmail.com>
2019-01-24 11:02:02 +0100

    finish part1

--- src/App.js
+++ src/App.js
@@ -9,12 +9,14 @@ class App extends Component {
       characters: []
 
     }
+
   }
 
 
 
   componentDidMount(){
     this.getApiCharacters();
+
   }
  
 
@@ -34,23 +36,21 @@ class App extends Component {
     
   }
   render() {
-    const {data}=this.state
-    console.log (data.characters);
     return (
       <div className="app">
       <h1 className="app__title">Harry Potter Characters</h1>
       <ul className="app__list">
-        {/* {data.characters.map(item=>{
+        {this.state.characters.map(item=>{
           console.log(item)
-          // return(
-          //   <li className="app__list__character" id={item.id} key={item.id}>
-          //     <h2 className="character__name">{item.name}</h2>
-          //     <img src={item.image} alt ={item.name}/>
-          //     <p className="character__house">{`House : ${item.house}`}</p>
-          //     <p className="character__ancestry"></p>
-          //   </li>
-          // );
-        })} */}
+          return(
+            <li className="app__list__character" id={item.id} key={item.id}>
+              <h2 className="character__name">{item.name}</h2>
+              <img src={item.image} alt ={item.name}/>
+              <p className="character__house">{`House : ${item.house}`}</p>
+              <p className="character__ancestry">{`Ancestry : ${item.ancestry}`}</p>
+            </li>
+          );
+        })}
       </ul>
        ;P
       </div>
