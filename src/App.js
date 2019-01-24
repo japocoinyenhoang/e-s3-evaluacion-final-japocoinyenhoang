035a8095be214a9d73b0e99dcbe581ee5c4d841b
JapocoinYenHoang <japocoin@gmail.com>
2019-01-24 10:31:04 +0100

    map elements

--- src/App.js
+++ src/App.js
@@ -7,8 +7,10 @@ class App extends Component {
     super(props);
     this.state={
       characters: []
+
     }
   }
+
 
 
   componentDidMount(){
@@ -22,15 +24,34 @@ class App extends Component {
       const newData=data.map((item, index)=>{
         return {...item, id:index};
       });
+      console.log(newData);
+
       this.setState({
         characters: newData
       })
+
     })
     
   }
   render() {
+    const {data}=this.state
+    console.log (data.characters);
     return (
-      <div className="App">
+      <div className="app">
+      <h1 className="app__title">Harry Potter Characters</h1>
+      <ul className="app__list">
+        {/* {data.characters.map(item=>{
+          console.log(item)
+          // return(
+          //   <li className="app__list__character" id={item.id} key={item.id}>
+          //     <h2 className="character__name">{item.name}</h2>
+          //     <img src={item.image} alt ={item.name}/>
+          //     <p className="character__house">{`House : ${item.house}`}</p>
+          //     <p className="character__ancestry"></p>
+          //   </li>
+          // );
+        })} */}
+      </ul>
        ;P
       </div>
     );
