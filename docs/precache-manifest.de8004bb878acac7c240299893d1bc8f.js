ba31ab741521d65a0255c9f87053b0c669e6d891
JapocoinYenHoang <japocoin@gmail.com>
2019-01-25 13:51:32 +0100

    create build docs

---
+++ docs/precache-manifest.de8004bb878acac7c240299893d1bc8f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "c4020bd1994878f71335f63877c8b7de",
+    "url": "./static/media/noAlivePic.c4020bd1.png"
+  },
+  {
+    "revision": "b5db285108bd3dd6fe1c97c9720cc1d1",
+    "url": "./static/media/HPmain.b5db2851.jpg"
+  },
+  {
+    "revision": "1c3cf503a23ab5a88d40adac88588bb6",
+    "url": "./static/media/HPGroup.1c3cf503.jpeg"
+  },
+  {
+    "revision": "0421d4186d6efbfc5331fe180895e780",
+    "url": "./static/media/HARRP___.0421d418.TTF"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "9ed8393f61e4cda6e577",
+    "url": "./static/js/main.9ed8393f.chunk.js"
+  },
+  {
+    "revision": "b375d8e077b53db46668",
+    "url": "./static/js/1.b375d8e0.chunk.js"
+  },
+  {
+    "revision": "9ed8393f61e4cda6e577",
+    "url": "./static/css/main.4c7b3217.chunk.css"
+  },
+  {
+    "revision": "1e359dc079ef1182ec1c6bd270a9206f",
+    "url": "./index.html"
+  }
+];
